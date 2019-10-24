a2bb86f61b11abdbed9661d9b8fb57f0c22a12f4
cngoEnsite <cngo@ensiteusa.com>
2019-10-24 10:46:17 -0500

    Change on rebaseTest

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -40,5 +40,19 @@ namespace wiserent.Controllers
                 }
             }
         }
+        public class WeatherTestRebase
+        {
+            public string DateFormatted { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+
+            public int TemperatureF
+            {
+                get
+                {
+                    return 32 + (int)(TemperatureC / 0.5556);
+                }
+            }
+        }
     }
 }
